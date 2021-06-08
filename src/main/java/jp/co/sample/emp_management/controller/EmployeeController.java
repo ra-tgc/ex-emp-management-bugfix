ba037bf0ab3dd5b72c4d5ff0b1690da1bee0660d
ra-tgc <masaki.taguchi@rakus.co.jp>
2021-06-08 11:51:05 +0900

    insertメソッドを追加

--- src/main/java/jp/co/sample/emp_management/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/emp_management/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package jp.co.sample.emp_management.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,7 +27,7 @@ public class EmployeeController {
 
 	@Autowired
 	private EmployeeService employeeService;
-	
+
 	/**
 	 * 使用するフォームオブジェクトをリクエストスコープに格納する.
 	 * 
@@ -53,14 +54,13 @@ public class EmployeeController {
 		return "employee/list";
 	}
 
-	
 	/////////////////////////////////////////////////////
 	// ユースケース：従業員詳細を表示する
 	/////////////////////////////////////////////////////
 	/**
 	 * 従業員詳細画面を出力します.
 	 * 
-	 * @param id リクエストパラメータで送られてくる従業員ID
+	 * @param id    リクエストパラメータで送られてくる従業員ID
 	 * @param model モデル
 	 * @return 従業員詳細画面
 	 */
@@ -70,26 +70,51 @@ public class EmployeeController {
 		model.addAttribute("employee", employee);
 		return "employee/detail";
 	}
-	
+
 	/////////////////////////////////////////////////////
 	// ユースケース：従業員詳細を更新する
 	/////////////////////////////////////////////////////
 	/**
 	 * 従業員詳細(ここでは扶養人数のみ)を更新します.
 	 * 
-	 * @param form
-	 *            従業員情報用フォーム
+	 * @param form 従業員情報用フォーム
 	 * @return 従業員一覧画面へリダクレクト
 	 */
 	@RequestMapping("/update")
 	public String update(@Validated UpdateEmployeeForm form, BindingResult result, Model model) {
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			return showDetail(form.getId(), model);
 		}
 		Employee employee = new Employee();
 		employee.setId(form.getIntId());
 		employee.setDependentsCount(form.getIntDependentsCount());
 		employeeService.update(employee);
+		return "redirect:/employee/showList";
+	}
+
+	/**
+	 * 従業員情報を登録します.
+	 * 
+	 * @param form 管理者情報用フォーム
+	 * @return ログイン画面へリダイレクト
+	 * 
+	 * @param form   従業員情報用フォーム
+	 * @param result エラー情報格納用オブジェクト
+	 * @param model  リクエストスコープ
+	 * @return 従業員一覧画面
+	 */
+	@RequestMapping("/insert")
+	public String insert(@Validated InsertEmployeeForm form, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return "employee/insert";
+		}
+
+		Employee employee = new Employee();
+		BeanUtils.copyProperties(form, employee);
+		employee.setHireDate(java.sql.Date.valueOf(form.getHireDate()));
+		employee.setSalary(Integer.parseInt(form.getSalary()));
+		employee.setDependentsCount(Integer.parseInt(form.getDependentsCount()));
+
 		return "redirect:/employee/showList";
 	}
 }
