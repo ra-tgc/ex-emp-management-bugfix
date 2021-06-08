a453ba8bbda8a6b6df070d8a4c8d4824a03e8b5f
ra-tgc <masaki.taguchi@rakus.co.jp>
2021-06-08 15:40:50 +0900

    郵便番号から住所を自動取得する機能追加

---
+++ src/main/resources/static/js/insert-employee.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+ "use strict";
+ $(function(){
+	$("#get-address-button").on("click", function(){
+		$.ajax({
+			url: "http://zipcoda.net/api",
+			dataType: "jsonp",
+			data: {
+				zipcode: $("#zipCode").val()
+			},
+			async: true
+		}).done(function(data){
+			console.dir(JSON.stringify(data));
+			$("#address").val(data.items[0].address); // 住所欄に住所をセット
+		}).fail(function(XMLHttpRequest, textStatus, errorThrown){
+			alert("正しい結果を得られませんでした。");
+			console.log("XMLHttpRequest：" + XMLHttpRequest.status);
+			console.log("textStatus：" + textStatus);
+			console.log("errorThrown：" + errorThrown.message);
+		});
+	});
+});
